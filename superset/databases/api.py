a4f22f409c6f0f0a484a9210b86d8c05baceea2a
kamalritwik <106542808+kamalritwik@users.noreply.github.com>
2022-06-18 21:36:39 +0530

    Update api.py
    Added the grid details and changed the view to accommodate the new tables and formatted the JSON.

--- superset/databases/api.py
+++ superset/databases/api.py
@@ -489,50 +489,54 @@ class DatabaseRestApi(BaseSupersetModelRestApi):
             if not database:
                 return self.response_404()
             result = database.select_star(
-                'target_table_col_def', schema_name, latest_partition=True,
+                'target_table_col_def_1', schema_name, latest_partition=True,
                 show_cols=True
             )
             df = database.get_df(result, schema_name)
             if df.empty:
                 return {}
-            group_table_name = df.groupby(['target_schema_id', 'table_name'])
-            unq_schemas = df['target_schema_id'].unique()
-            gb_groups = group_table_name.groups
-            list_req = list(gb_groups.keys())
-            if len(list_req) <= 0:
-                return {}
-            table_payload: List[Dict[str, Any]] = []
-            payload_tables: List[Dict[str, Any]] = []
-            for k in list_req:
-                payload_columns: List[Dict[str, Any]] = []
-                for i in range(len(df.loc[gb_groups[k]])):
-                    payload_columns.append({
-                        "name": df.loc[gb_groups[k][i]]['col_name'],
-                        "type": df.loc[gb_groups[k][i]]['data_type'],
-                        "longType": df.loc[gb_groups[k][i]]['data_type'],
-                        "keys": [],
-                        "comment": None})
-                table = {
-                    "value": df.loc[gb_groups[k][i]]['table_name'],
-                    "schema": schema_name,
-                    "title": df.loc[gb_groups[k][i]]['table_name'],
-                    "label": df.loc[gb_groups[k][i]]['table_name'],
-                    "type": 'table',
-                    "extra": None,
-                    "columns": payload_columns}
-                payload_tables.append(table)
-            schema = {
-                "value": k[0],
-                "schema": schema_name,
-                "type": 'schema',
-                "tables": payload_tables,
+            group_grid_name = df.groupby(['gridConfigurationId'])
+            unq_schemas = df['gridConfigurationId'].unique()
+            schema_name_payload: List[Dict[str, Any]] = []
 
-            }
-            table_payload.append(schema)
+            for name, group in group_grid_name:
+                group_table_name = group.groupby('targetTableName')
+                payload_tables: List[Dict[str, Any]] = []
+                for (idx, table_row) in group_table_name:
+                    payload_columns: List[Dict[str, Any]] = []
+                    for i, row in table_row.iterrows():
+                        grid_name = row['gridConfigurationName']
+                        payload_columns.append({
+                            "name": row['columnName'],
+                            "type": row['DataTypeName'],
+                            "longType": row['DataTypeName'],
+                            "keys": [],
+                            "comment": None})
+                    table_name = idx
+                    table = {
+                        "value": table_name,
+                        "schema": schema_name,
+                        "title": table_name,
+                        "label": table_name,
+                        "type": 'table',
+                        "extra": None,
+                        "columns": payload_columns}
+
+                    payload_tables.append(table)
+                schema = {
+                    "value": name,
+                    "schema": grid_name,
+                    "type": 'schema',
+                    "tables": payload_tables,
+
+                }
+
+                schema_name_payload.append(schema)
             table_final_payload = {
                 "schemalength": len(unq_schemas),
-                "options": table_payload
+                "options": schema_name_payload
             }
+
             return json_success(json.dumps(table_final_payload))
         except SQLAlchemyError as ex:
             self.incr_stats("error", self.table_metadata.__name__)
